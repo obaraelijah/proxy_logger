ce3e93ad2ca273ff0e664ed6434e64f21d2cd983
obaraelijah <elijahobara357@gmail.com>
2024-06-20 14:57:00 +0300

    feat: Implement logging configuration & cli parser

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
-#[tokio::main]
+use std::env;
+
+use clap::Parser;
+use proxy_logger::Arguments;
+
+#[tokio::main(flavor = "multi_thread", worker_threads = 4)]
 async fn main() {
+    let arguments = Arguments::parse();
+
+    env::set_var("RUST_LOG", arguments.level.to_string());
     env_logger::builder()
         .parse_default_env()
         .format_target(false)
