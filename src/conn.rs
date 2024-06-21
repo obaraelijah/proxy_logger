c69748fb735485f436db0bf9720fe088bce0cb2d
obaraelijah <elijahobara357@gmail.com>
2024-06-21 12:41:09 +0300

    feat: Resolve default constructed unit arg clippy lint

--- src/conn.rs
+++ src/conn.rs
@@ -1,6 +1,11 @@
 use crate::get_formatter_by_kind;
 use crate::Arguments;
 use bytes::BytesMut;
+use logged_stream::ConsoleLogger;
+use logged_stream::DefaultFilter;
+use logged_stream::LoggedStream;
+use logged_stream::RecordKind;
+use logged_stream::RecordKindFilter;
 use std::time::Duration;
 use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
 use tokio::net as tokio_net;
@@ -29,26 +34,36 @@ pub async fn initialize_tcp_listener(arguments: Arguments) {
 }
 
 async fn incoming_connection_handle(arguments: Arguments, source_stream: tokio_net::TcpStream) {
-    let (mut source_stream_read_half, mut source_stream_write_half) = io::split(
+    let (mut source_stream_read_half, mut source_stream_write_half) = io::split(LoggedStream::new(
         source_stream,
-        get_formatter_by_kind(arguments.formatting, arguments.separator.as_str())
-    );
+        get_formatter_by_kind(arguments.formatting, arguments.separator.as_str()),
+        DefaultFilter,
+        ConsoleLogger::new_unchecked("debug"),
+    ));
     let destination_stream = tokio_net::TcpStream::connect(arguments.remote_addr)
         .await
         .expect("Failed to connect to destination address");
     let (mut destination_stream_read_half, mut destination_stream_write_half) =
-        io::split(destination_stream);
+        io::split(LoggedStream::new(
+            destination_stream,
+            get_formatter_by_kind(arguments.formatting, arguments.separator.as_str()),
+            RecordKindFilter::new(&[RecordKind::Drop, RecordKind::Error, RecordKind::Shutdown]),
+            ConsoleLogger::new_unchecked("debug"),
+        ));
 
     let destination_stream_handle = tokio::spawn(async move {
         let mut buffer = BytesMut::with_capacity(2048);
         'destination_stream_handle: loop {
-            let Ok(read_length) = destination_stream_read_half.read_buf(&mut buffer).await else { 
+            let Ok(read_length) = destination_stream_read_half.read_buf(&mut buffer).await else {
                 break 'destination_stream_handle;
             };
             if read_length == 0 {
                 continue 'destination_stream_handle;
             }
-            let Ok(write_length) = source_stream_write_half.write(&buffer[0..read_length]).await else {
+            let Ok(write_length) = source_stream_write_half
+                .write(&buffer[0..read_length])
+                .await
+            else {
                 break 'destination_stream_handle;
             };
             assert_eq!(read_length, write_length);
@@ -61,15 +76,20 @@ async fn incoming_connection_handle(arguments: Arguments, source_stream: tokio_n
         'source_stream_handle: loop {
             let Ok(Ok(read_length)) = timeout(
                 Duration::from_secs(arguments.timeout),
-                source_stream_read_half.read_buf(&mut buffer)
-            ).await else {
+                source_stream_read_half.read_buf(&mut buffer),
+            )
+            .await
+            else {
                 destination_stream_handle.abort();
                 break 'source_stream_handle;
             };
             if read_length == 0 {
                 continue 'source_stream_handle;
             }
-            let Ok(write_length) = destination_stream_write_half.write(&buffer[0..read_length]).await else {
+            let Ok(write_length) = destination_stream_write_half
+                .write(&buffer[0..read_length])
+                .await
+            else {
                 destination_stream_handle.abort();
                 break 'source_stream_handle;
             };
